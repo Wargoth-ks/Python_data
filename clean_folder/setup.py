9df900c16dcad0392bcc85b5cbd622c9cef5b751
Wargoth_ks <Wargoth-ks@users.noreply.github.com>
2023-04-10 17:04:37 +0300

    New_commit

---
+++ clean_folder/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="clean_folder",
+    version="0.1.0",
+    packages=find_packages(),
+    description='This program sorting thrash folder',
+    url='https://github.com/Wargoth-ks/Python_data/clean_folder',
+    author='Wargoth_ks',
+    author_email='warheart1986@gmail.com',
+    license='MIT',
+    entry_points={
+        "console_scripts": [
+            "clean-folder = clean_folder.clean_folder:main"
+        ],
+    },
+)
