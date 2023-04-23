20b5661ac32ad6fd66295a5d24b2de01dd05835d
Wargoth_ks <Wargoth-ks@users.noreply.github.com>
2023-04-23 12:49:13 +0300

    Update B_days.py

--- Birthday_Middle_Earth/B_days.py
+++ Birthday_Middle_Earth/B_days.py
@@ -1,26 +1,38 @@
 from datetime import datetime as _dtime, timedelta as _delta
 
 def get_birthdays_per_week(users):
-    today = _dtime.now()
-    next_week = today + _delta(days=7)
+
+    today = _dtime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    prev_sat = (today - _delta(days=2)).replace(hour=0, minute=0, second=0, microsecond=0)
+    prev_sun = (today - _delta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
+    week_delta = (today + _delta(days=6)).replace(hour=0, minute=0, second=0, microsecond=0)
     w_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     b_day_dict = {day: [] for day in w_days}
 
     for user in users:
         b_day = user['birthday'].replace(year=today.year)
-        if b_day < today:
-            b_day = b_day.replace(year=today.year+1)
-        elif b_day > next_week:
-            b_day = b_day.replace(year=today.year-1)
         w_day = b_day.strftime('%A')
-        if w_day in ['Saturday', 'Sunday']:
-            w_day = 'Monday'
+
+        # If birthdays were on Saturday and Sunday, then we add them to Monday
+        if b_day == prev_sun or (b_day == prev_sat and today.weekday() == 0) \
+                            or (b_day == (prev_sun + _delta(days=1))):
+            w_day = "Monday"
+
+        # Check if birthdays have been already passed in current year
+        elif today.replace(year=today.year, month=1, day=1) <= b_day < today \
+                            and (b_day.weekday() < 5):
+            continue
+
+        # We check if the birthday will occur more than a week from now 
+        elif b_day >= week_delta or (b_day.weekday() >= 5 \
+                    and b_day not in [prev_sat, prev_sun]):
+            continue
+        
         b_day_dict[w_day].append(user['name'])
 
     for day in w_days:
         if b_day_dict[day]:
             print(f"{day}: {', '.join(b_day_dict[day])}")
-
             
     
 
