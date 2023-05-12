53504fc6e2a26b6351666e53d25601706be07d1d
Wargoth_ks <Wargoth-ks@users.noreply.github.com>
2023-05-13 02:35:54 +0300

    New_mod_ver

---
+++ cli_bot_classes_ver_8.4/classes.py
@@ -0,0 +1,186 @@
+from collections import UserDict
+
+from datetime import date
+import atexit
+import pickle
+import os
+
+FILENAME = 'contacts_saved.pickle'
+
+class Field:
+
+    def __init__(self, value=None):
+        self.value = value
+
+class Name(Field):
+
+    def __init__(self, value) -> None:
+        super().__init__(value)
+        self._private_name = value
+
+    def __repr__(self) -> str:
+        return f"{self.value}"
+
+    @property
+    def value(self):
+        return self._private_name
+    
+    @value.setter
+    def value(self, arg_name):
+        if 3 <= len(arg_name) <= 22:
+            self._private_name = arg_name
+        else:
+            raise ValueError(f"Name must have more then 2 symbols and less the 22 symbols")
+        
+
+class Phone(Field):
+
+    def __init__(self, value) -> None:
+        super().__init__(value)
+        self._private_phone = value
+
+    def __repr__(self) -> str:
+        return f"{self.value}"
+
+    @property
+    def value(self):
+        return self._private_phone
+    
+    @value.setter
+    def value(self, arg_phone):
+        if 10 <= len(str(arg_phone)) <= 17:
+            self._private_phone = arg_phone
+        else:
+            raise ValueError(f"Lenght of phone number must be 10-17 symbols")
+
+
+class Birthday(Field):
+    
+    def __init__(self, value):
+        super().__init__(value)
+        self._private_bd = value
+
+    def __repr__(self) -> str:
+        return f"{self.value}"
+    
+    @property
+    def value(self):
+        return self._private_bd
+    
+    @value.setter
+    def value(self, arg_bd: date):
+        if isinstance(arg_bd, date):
+            self._private_bd = arg_bd
+        else:
+            raise ValueError("Date of BD must be in format << dd.mm.yyyy >>")
+    
+
+class Record(Field):
+
+    def __init__(self, name: Name, phones: list[Phone]=[], birthday=None):
+        self.name = name
+        self.phones = phones
+        self.birthday = birthday
+
+    def __getitem__(self, key):
+        if key == 'phones':
+            return self.phones
+        elif key == 'birthday':
+            return self.birthday
+        else:
+            raise KeyError(key)
+
+    def add_phone(self, phone: Phone):
+        self.phones.insert(0, phone)
+
+    def edit_phone(self, phone: Phone, new_value: Phone):
+        for p in self.phones:
+            if p == phone:
+                p.value = new_value
+                break
+
+    def delete_phone(self, phone: Phone):
+        if phone not in self.phones:
+            raise ValueError(f"<< {self.name} >> doesn't have << phone: {phone} >>")
+        self.phones.remove(phone)
+
+    def days_to_birthday(self, birthday):
+        self.birthday = birthday
+        if self.birthday is None:
+            return "No BD"
+        today = date.today()
+        print(today)
+        next_bd = date(today.year, self.birthday.value.month, self.birthday.value.day)
+        if next_bd < today:
+            next_bd = next_bd.replace(year=today.year + 1)
+        delta = next_bd - today
+        return f"Days until birthday: {delta.days}"
+    
+    def __repr__(self) -> str:
+        return f"{self.name!r}: {self.phones!r}, {(self.birthday)}"
+
+class AddressBook(UserDict):
+
+    def __init__(self):
+        self.data = {}
+
+        if os.path.exists(FILENAME):
+            with open(FILENAME, 'rb') as f:
+                loaded_data = pickle.load(f)
+                self.data.update(loaded_data)
+        atexit.register(self.save_contacts_on_exit)
+    
+
+    def save_contacts_on_exit(self):
+        with open(FILENAME, 'wb') as f:
+            pickle.dump(self.data, f)
+
+    def add_record(self, record: Record):
+        if record.name.value not in self.data: # Record(name,phone,birth)
+            self.data[record.name.value] = record
+
+
+    def delete_record(self, name: Name):
+        if name.value not in self.data:
+            raise ValueError(f"<< {name.value} >> not found in phone book")
+        del self.data[name.value]
+
+    def update_record(self, old_name, new_name):
+        if old_name.value not in self.data:
+            raise ValueError(f"<< {old_name.value} >> not found in phone book")
+        record = self.data.pop(old_name.value)
+        record.name = new_name
+        self.data[new_name.value] = record
+
+    def find_records(self, name=None, phone=None, birthday=None):
+        if name is not None:
+            return [self.data.get(name, [])]
+        elif phone is not None:
+            found_records = []
+            for record in self.data.values():
+                for p in record.phones:
+                    if p.value == phone.value:
+                        found_records.append(record)
+                        break
+            return found_records[0]
+        elif birthday is not None:
+            return [record for record in self.data.values() if record['birthday'] == birthday]
+    
+    def print_records(self, records):
+        for record in records:
+            name, info = record
+            print(f"Name: {name}")
+            phones = [str(phone) for phone in info['phones']]
+            print(f"Phone: {', '.join(phones)}")
+            if info['birthday']:
+                print(f"Birthday: {info['birthday'].value.strftime('%d.%m.%Y')}")
+            else:
+                print("Birthday: No info")
+            print()
+
+    def iterator(self, n: int):
+        records = sorted(list(self.data.items()))
+        i = 0
+        while i < len(records):
+            yield records[i:i+n]
+            i += n
---
+++ cli_bot_classes_ver_8.4/main.py
@@ -0,0 +1,80 @@
+from termcolor import colored
+from show_greeting import greet
+from datetime import datetime
+from commands import hello, helper, add_contact, del_contact, del_phone, error_func
+from commands import change_phone, show_all, good_bye, add_phone, change_name, add_bd, get_bd,  search_by_name, search_by_phone
+import re
+
+cmds = {
+    "hello": hello,
+    "help": helper,
+    "add": add_contact, # >= 2 arg name phone (bd) +
+    "add_phone": add_phone, # 2 arg name phone +
+    "del": del_contact, # 1 arg name +
+    "del_phone": del_phone, # 2 arg name phone +-
+    "change": change_name, # 2 arg old_name new_name
+    "change_phone": change_phone, # 2 arg name new_phone + digits +
+    "show all": show_all, # +
+    "add_bd": add_bd, # >= 2 arg name bd + dates -
+    "get_bd": get_bd, # >= 1 arg + 
+    "by_name":  search_by_name, # 1 or 2 arg +
+    "by_phone": search_by_phone, # 1 or 2 arg +
+    "good bye": good_bye, # +
+    "close": good_bye, # +
+    "exit": good_bye, # +
+    ".": good_bye # +
+}
+
+list_cmds = ["show all", "good bye", "close", "exit", ".", "help", "hello"]
+
+
+def main():
+    while True:
+        usr_inp = input(colored("\nEnter command: ", "green"))
+        if usr_inp in list_cmds:
+            s_func = cmds[usr_inp.strip()]
+            print(s_func())
+        elif not usr_inp or usr_inp.split()[0] not in cmds.keys():
+            print(error_func())
+        else:
+            if usr_inp.split()[0] in cmds.keys() and len(usr_inp.split()) > 1:
+                command, *data = usr_inp.strip().split(' ', 1)
+                handler = cmds[command]
+                args = data[0].split(" ") # data[0]
+                #print(command, args)
+                if not data:
+                    print(error_func())
+                    continue
+                if len(data[0].split(", ")) == 2: # old_name new_name
+                    arg_1, arg_2 = data[0].split(", ")
+                    print(handler(arg_1, arg_2))
+                    continue
+                if len(args) >= 2:
+                    if all(symb.isdigit() or symb in ['+', '-'] for symb in args[-2]): # add name phone birthday
+                        date_arg = args[-1] # birthday
+                        d_arg = datetime.strptime(date_arg, '%d.%m.%Y').date()
+                        dig_arg = str(args[-2]) # phone
+                        list_args = [" ".join(args[:-2]), dig_arg, d_arg] # [phone, birthday]
+                        print(handler(*list_args))
+                        continue
+                    elif all(symb.isdigit() or symb in ['+', '-'] for symb in args[-1]): # add name phone; change_phone name phone; del_phone name phone;
+                        dig_arg = str(args[-1]) # phone
+                        list_args = [" ".join(args[:-1]), dig_arg]
+                        print(handler(*list_args))
+                        continue
+                if len(args) >= 1 and re.match(r'^\d{2}\.\d{2}\.\d{4}$',args[-1]) is None: # by_phone, by_name
+                    #print(data)
+                    print(handler(*data))
+                    continue
+                else:
+                    date_arg = datetime.strptime(args[-1], '%d.%m.%Y').date()
+                    list_args = [" ".join(args[:-1]), date_arg] # get_bd
+                    #print(list_args)
+                    print(handler(*list_args))
+            else:
+                print("Type Error: " + colored(f"Command << {usr_inp} >> must have any arguments !!!", "red"))
+
+
+if __name__ == "__main__":
+    greet()
+    main()
