226689943a04e002f4640603a2011c749f0aa673
Wargoth_ks <Wargoth-ks@users.noreply.github.com>
2023-05-19 17:01:47 +0300

    RC or R?

---
+++ cli_bot_classes_ver_10_3/commands.py
@@ -0,0 +1,160 @@
+from decorator import input_error, validation_parametrs, validation_exists_name
+from show_help import show_help
+from classes import AddressBook, Name, Phone, Birthday, Record
+from termcolor import colored
+
+# Processing input commands
+
+contacts = AddressBook()
+
+@input_error
+def hello():
+    return "\nHow can I help you?"
+
+@input_error
+def good_bye():
+    print("\nBye bye!")
+    exit()
+
+@input_error
+def helper():
+    return show_help()
+
+@input_error
+def error_func():
+    raise ValueError(f"Command not found. Please, try again!")
+
+@input_error
+@validation_parametrs
+def add_contact(name, phone, birthday=None):
+    name = Name(name)
+    phone = Phone(phone)
+    if birthday is not None:
+        birthday = Birthday(birthday)
+        record = Record(name, phones=[phone], birthday=birthday)
+    else:
+        record = Record(name, phones=[phone])
+    contacts.add_record(record)
+    return f"<< Contact {name} >> has been added to the phone book"
+
+@input_error
+@validation_parametrs
+@validation_exists_name(contacts)
+def add_phone(name, phone):
+    phone = Phone(phone)
+    name = Name(name)
+    contact = contacts[name.value]
+    contact.add_phone(str(phone.value))
+    return f"<< Number {phone} >> has been added to the contact << {name} >>"
+
+@input_error
+@validation_parametrs
+@validation_exists_name(contacts)
+def add_bd(name, birthday):
+    name = Name(name)
+    birthday_obj = Birthday(birthday)
+    record = contacts.data[name.value]
+    if record['birthday'] is not None:
+        raise ValueError(f"Contact << {name} >> already have birthday!!!")
+    record.add_birthday(birthday_obj) 
+    return f"<< Birthday >> has been added to the contact << {name} >>"
+    
+@input_error
+@validation_parametrs
+@validation_exists_name(contacts)
+def del_contact(name):
+    name = Name(name)
+    contacts.delete_record(name)
+    return f"<< {name} >> has been removed from phone book"
+
+@input_error
+@validation_parametrs
+@validation_exists_name(contacts)
+def del_phone(name, phone):
+    phone = Phone(phone)
+    name = Name(name)
+    contact = contacts[name.value]
+    contact.delete_phone(phone)
+    return f"<< Number {phone} >> has been removed from phone book"
+
+@input_error
+@validation_parametrs
+@validation_exists_name(contacts)
+def del_bd(name):
+    name = Name(name)
+    record = contacts.data[name.value]
+    record.delete_birthday()
+    return f"<< Birthday >> succefully deleted from contact << {name} >>"
+        
+@input_error
+@validation_parametrs
+@validation_exists_name(contacts)
+def change_name(name, new_name):
+    name = Name(name)
+    new_name = Name(new_name)
+    contacts.update_record(name, new_name)
+    return f"Name for << {name.value} >> has been updated to << {new_name.value} >>"
+
+@input_error
+@validation_parametrs
+@validation_exists_name(contacts)
+def change_phone(name, phone):
+    name = Name(name)
+    phone = Phone(phone)
+    record = contacts.data[name.value]
+    record.edit_phone(record.phones[0], phone)
+    return f"Phone number for << contact {name.value} >> has been updated"
+
+@input_error
+@validation_parametrs
+@validation_exists_name(contacts)
+def upd_bd(name, birthday, phone=None):
+    name = Name(name)
+    birthday = Birthday(birthday)
+    if phone is None:
+        record = contacts.data[name.value]
+        record.update_birthday(birthday) 
+        return f"<< Birthday >> succefuly updated to contact << {name} >>"
+        
+@input_error
+@validation_parametrs
+@validation_exists_name(contacts)
+def get_bd(name):
+    name = Name(name)
+    record = contacts.data[name.value]
+    days_to_bd = record.days_to_birthday(record['birthday'])
+    return days_to_bd
+    
+@input_error    
+def search_by_name(name):
+    name_obj = Name(name)
+    find_name = contacts.find_records(name=name_obj)
+    if find_name:
+        return find_name
+    else:
+        return f"No records found with name: {name_obj}"
+
+@input_error
+def search_by_phone(phone):
+    phone_obj = Phone(phone)
+    find_phone = contacts.find_records(phone=phone_obj)
+    if find_phone:
+        return find_phone
+    else:
+        return f"No records found with phone number: {phone_obj}"
+
+@input_error 
+def show_all():
+    # print(contacts)
+    usr_input = input(colored("\nEnter number of records: ", "magenta"))
+    if usr_input == "":
+        contacts.print_records(sorted(contacts.items()))
+        return ""
+    else:
+        n = int(usr_input)
+        for records in sorted(contacts.iterator(n)):
+            contacts.print_records(records)
+            usr_input = input("\nPress enter to show more records or 'q' to quit: ")
+            if usr_input == "q":
+                return "Done!"
+    return "No more records"
---
+++ cli_bot_classes_ver_10_3/show_help.py
@@ -0,0 +1,46 @@
+# This function show help in table
+
+from termcolor import colored
+
+def show_help():
+
+    cmd_help = {
+        "hello": "Say hello",
+        "help": "Show this help",
+        "add <name> <number> <birthday>": "Add contact, number, birthday",
+        "add_phone <name> <number>": "Add new number to contact",
+        "add_bd <name> <birthday>": "Add birthday - DD.MM.YYY to contact",
+        "del <name>": "Remove contact",
+        "del_phone <name> <number>": "Remove contact's number",
+        "del_bd <name>": "Remove contact's birthday - DD.MM.YYYY",
+        "change <old name>, <new_name>": "Update contact's name",
+        "change_phone <name> <new number>": "Change contact's phone number",
+        "upd_bd <name> <birthday>": "Change contact's birthday - DD.MM.YYYY",
+        "get_bd <name>": "Get days to contact's birthday - DD.MM.YYYY",
+        "name <name>": "Search contact's info by name",
+        "phone <phone>": "Search contact's info by phone",
+        "show all": "Show all contacts",
+        "good bye, exit, close or .": "Exit program"
+    }
+
+    max_name_len = max([len(cmds) for cmds in cmd_help.keys()] + [4])
+    max_phone_len = max([len(desc) for desc in cmd_help.values()] + [6])
+
+    # Header of table
+    table = f'+{"-" * (max_name_len + 2)}+{"-" * (max_phone_len + 2)}+\n'
+    table += "|{:^62}|\n".format("Bot's command")
+
+    # Header of table
+    table += f'+{"-" * (max_name_len + 2)}+{"-" * (max_phone_len + 2)}+\n'
+    table += f'| {"Command":<{max_name_len}} | {"Description":<{max_phone_len}} |\n'
+    table += f'+{"-" * (max_name_len + 2)}+{"-" * (max_phone_len + 2)}+\n'
+
+    # Data of table
+    for cmd, desc in cmd_help.items():
+        table += f'| {cmd:<{max_name_len}} | {desc:<{max_phone_len}} |\n'
+
+    # Generating the table footer
+    table += f'+{"-" * (max_name_len + 2)}+{"-" * (max_phone_len + 2)}+'
+    return colored(table, "magenta")
+
+print()
